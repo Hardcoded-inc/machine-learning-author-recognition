575066adfcbe1630d7977ab16bab47b7fedb5f36
Jakub Macioszek <kmacioszek99@gmail.com>
2021-07-04 01:08:21 +0200

    Add some normalizer methods tests

--- __test__/normalizer_test.py
+++ __test__/normalizer_test.py
@@ -27,3 +27,30 @@ class NormalizerTest(TestCase):
 
         result_val = "Some  text with punctuation Ill puthere an  what about questions God It should work NICE"
         self.assertEqual(df.iloc[0]["some_col"], result_val )
+
+    def test_remove_phrases_with_numbers(self):
+        df = pd.DataFrame({"some_col": ["123456789 some text with numbers I_should_be_deleted_2 987654321"]})
+        self.normalizer.remove_phrases_with_numbers(df, col="some_col")
+
+        result_val = " some text with numbers  "
+        self.assertEqual(df.iloc[0]["some_col"], result_val)
+
+    def test_drop_empty_records(self):
+        df = pd.DataFrame({"some_col": ["abc", '', '']})
+        df = self.normalizer.drop_empty_records(df, col="some_col")
+
+        self.assertEqual(len(df.some_col), 1)
+
+    def test_drop_written_by(self):
+        df = pd.DataFrame({"some_col": ["written by some author", 'something is being written', 'made by me']})
+        df = self.normalizer.drop_written_by(df, col="some_col")
+
+        self.assertEqual(len(df.some_col), 2)
+
+    def test_remove_stop_words(self):
+        df = pd.DataFrame({"some_col": ['All of the stopwords should do a magic trick and disappear.']})
+        self.normalizer.remove_stop_words(df, col="some_col")
+
+        result_val = 'All stopwords magic trick disappear.'
+
+        self.assertEqual(df.iloc[0]["some_col"], result_val)
--- normalizer.py
+++ normalizer.py
@@ -13,14 +13,12 @@ class Normalizer:
         self.verbose = flags["verbose"]
 
     def lowercase(self, df, col="seq"):
-        if(self.verbose):
-            print(f"- Lowercasing.")
+        self.print_current_step(f"- Lowercasing.")
 
         df[col] = df[col].apply(lambda text: text.lower())
 
     def remove_inbrackets_text(self, df, col="seq"):
-        if(self.verbose):
-            print(f"- Removing inbrackets text.")
+        self.print_current_step(f"- Removing inbrackets text.")
 
         # remove brackets/parenthesis/braces
         df[col] = df[col].str.replace('\[.*\]', '')
@@ -32,37 +30,31 @@ class Normalizer:
 
 
     def remove_punctuations(self, df, col="seq"):
-        if(self.verbose):
-            print(f"- Removing punctuation.")
+        self.print_current_step(f"- Removing punctuation.")
 
         df[col] = df[col].str.replace('[^\w\s]|_', '')
 
 
     def remove_phrases_with_numbers(self, df, col="seq"):
-        if(self.verbose):
-            print(f"- Removing phrases with numbers.")
+        self.print_current_step(f"- Removing phrases with numbers.")
 
         df[col] = df[col].str.replace('\w*\d\w*', '')
 
 
 
     def drop_empty_records(self, df, col="seq"):
-        if(self.verbose):
-            print(f"- Dropping empty records.")
+        self.print_current_step(f"- Dropping empty records.")
 
-        df = df[df[col].str.contains("[a-zA-Z]")]
-
+        return df[df[col].str.contains("[a-zA-Z]")]
 
     def drop_duplicates(self, df, col="seq"):
-        if(self.verbose):
-            print(f"- Dropping duploicated.")
+        self.print_current_step(f"- Dropping duploicated.")
 
         df.drop_duplicates(inplace=True)
 
 
     def drop_nans(self, df, col="seq"):
-        if(self.verbose):
-            print(f"- Dropping NaNs.")
+        self.print_current_step(f"- Dropping NaNs.")
 
         nan_value = float("NaN")
         df.replace("", nan_value, inplace=True)
@@ -70,11 +62,10 @@ class Normalizer:
 
 
     def drop_written_by(self, df, col="seq"):
-        if(self.verbose):
-            print(f"- Dropping 'written by' rows.")
+        self.print_current_step(f"- Dropping 'written by' rows.")
 
         # Remove written by
-        df = df[~df[col].str.contains("written by")]
+        return df[~df[col].str.contains("written by")]
         # df[col] = df[col].str.replace(r'written by.*(?=\.\.)\.\.', '')
 
 
